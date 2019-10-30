83bf205ad1e6b4b372635782abbddef97b150a0c
Bridger Reif Hammond <bridgerrhammond@gmail.com>
2019-10-30 11:19:28 -0500

    sorted tests done

--- test/app.test.js
+++ test/app.test.js
@@ -19,8 +19,24 @@ describe('test the app', () => {
       .query({ sort: '' })
       .expect(400);
   });
-  // /apps?sort=app should return sorted apps by name
-  // /apps?sort=rating should return sorted apps by rating
+  it('/apps?sort=app should return sorted apps by name', () => {
+    return supertest(app)
+      .get('/apps')
+      .query({ sort: 'app' })
+      .then(res => {
+        expect(res.body[0].App).to.equal('Angry Birds Rio') &&
+          expect(res.body[res.body.length - 1].App).to.equal('slither.io');
+      });
+  });
+  it('/apps?sort=rating should return sorted apps by rating', () => {
+    return supertest(app)
+      .get('/apps')
+      .query({ sort: 'rating' })
+      .then(res => {
+        expect(res.body[0].Rating).to.equal(4.2) &&
+          expect(res.body[res.body.length - 1].Rating).to.equal(4.7);
+      });
+  });
   it('/apps?genre??? 400', () => {
     return supertest(app)
       .get('/apps')
