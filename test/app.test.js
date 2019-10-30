727d3ccd402fffdce7128d80ef27cfeff150811a
Bridger Reif Hammond <bridgerrhammond@gmail.com>
2019-10-30 10:53:49 -0500

    2 tests done

--- test/app.test.js
+++ test/app.test.js
@@ -1,11 +1,27 @@
 const supertest = require('supertest');
 const app = require('../app');
+const data = require('../playstore');
 const { expect } = require('chai');
 
-// /apps should return all [apps] + 200
-// /apps?sort____ 400
-// /apps?sort=app should return sorted apps by name
-// /apps?sort=rating should return sorted apps by rating
-// /apps?genre??? 400
-// /apps?genre(does exist) should return filtered list with only that genre
-// /apps?sort+genre should return filtered and sorted list
+describe('test the app', () => {
+  it('/apps should return all [apps] + 200', async () => {
+    return supertest(app)
+      .get('/apps')
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .then(res => {
+        expect(res.body).to.deep.equal(data);
+      });
+  });
+  it('/apps?sort____ 400', () => {
+    return supertest(app)
+      .get('/apps')
+      .query({ sort: '' })
+      .expect(400);
+  });
+  // /apps?sort=app should return sorted apps by name
+  // /apps?sort=rating should return sorted apps by rating
+  // /apps?genre??? 400
+  // /apps?genre(does exist) should return filtered list with only that genre
+  // /apps?sort+genre should return filtered and sorted list
+});
